7a7964f8e917750633b6b2698baf03c5ca851f4f
aseel <aseelfarrar1@hotmail.com>
2021-08-10 08:10:48 +0300

    lab-27 details and sitting (xml)

--- app/src/main/java/com/example/taskmaster/MainActivity.java
+++ app/src/main/java/com/example/taskmaster/MainActivity.java
@@ -3,8 +3,6 @@ package com.example.taskmaster;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
