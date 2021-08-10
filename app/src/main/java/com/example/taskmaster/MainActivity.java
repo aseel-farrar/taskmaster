6ab4bd8d9250462e2d3d186f113e5224d1dd5441
aseel <aseelfarrar1@hotmail.com>
2021-08-10 06:39:15 +0300

    lab-26

--- app/src/main/java/com/example/taskmaster/MainActivity.java
+++ app/src/main/java/com/example/taskmaster/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.taskmaster;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Go to add task activity
+        findViewById(R.id.buttonAddTaskActivity).setOnClickListener(view -> {
+            Intent goToAddTaskActivity = new Intent(MainActivity.this, AddTask.class);
+            startActivity(goToAddTaskActivity);
+        });
+
+        // Go to all tasks activity
+        findViewById(R.id.buttonAllTasks).setOnClickListener(view -> {
+            Intent goToAllTaskActivity = new Intent(MainActivity.this, AllTasks.class);
+            startActivity(goToAllTaskActivity);
+        });
     }
 }
