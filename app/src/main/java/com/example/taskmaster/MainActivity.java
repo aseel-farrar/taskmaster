bb4b2b9ad02e013690ff8321e86c92dc5c42f24f
aseel <aseelfarrar1@hotmail.com>
2021-08-10 20:21:53 +0300

    lab-27 all functionality done

--- app/src/main/java/com/example/taskmaster/MainActivity.java
+++ app/src/main/java/com/example/taskmaster/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.taskmaster;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         // Go to add task activity
         findViewById(R.id.buttonAddTaskActivity).setOnClickListener(view -> {
@@ -26,14 +31,43 @@ public class MainActivity extends AppCompatActivity {
 
         // Go to sittings activity
         findViewById(R.id.imageViewSittings).setOnClickListener(view -> {
-            Intent goToSittingsActivity = new Intent(MainActivity.this,Settings.class);
+            Intent goToSittingsActivity = new Intent(MainActivity.this, Settings.class);
             startActivity(goToSittingsActivity);
         });
+
+        // Handling the tasks buttons
+        findViewById(R.id.buttonCode).setOnClickListener(view -> {
+            showDetails(((Button)findViewById(R.id.buttonCode)).getText().toString());
+        });
+
+        findViewById(R.id.buttonClean).setOnClickListener(view -> {
+            showDetails(((Button)findViewById(R.id.buttonClean)).getText().toString());
+        });
+
+        findViewById(R.id.buttonSport).setOnClickListener(view -> {
+            showDetails(((Button)findViewById(R.id.buttonSport)).getText().toString());
+        });
+    }
+
+    /**
+     * function to call the details Activity for specific task Name
+     *
+     * @param taskName: taskName
+     */
+    public void showDetails(String taskName){
+        Intent intent = new Intent(MainActivity.this, Details.class);
+        intent.putExtra("taskName", taskName);
+        startActivity(intent);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-
+        //create sharedPreference
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        String username = sharedPreferences.getString("username", "");
+        if (!username.equals("")) {
+            ((TextView)findViewById(R.id.textViewTasks)).setText(username+"'s Tasks");
+        }
     }
 }
--- app/src/main/java/com/example/taskmaster/Settings.java
+++ app/src/main/java/com/example/taskmaster/Settings.java
@@ -1,7 +1,13 @@
 package com.example.taskmaster;
 
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Settings extends AppCompatActivity {
 
@@ -9,5 +15,20 @@ public class Settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        //create sharedPreference
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        SharedPreferences.Editor preferenceEditor = sharedPreferences.edit();
+
+        // save the user name in sharedPreference
+        findViewById(R.id.imageViewSave).setOnClickListener(view -> {
+            String username = ((EditText) findViewById(R.id.editTextUsername)).getText().toString();
+
+            preferenceEditor.putString("username", username);
+            preferenceEditor.apply();
+
+            Toast toast = Toast.makeText(this, "saved!", Toast.LENGTH_LONG);
+            toast.show();
+        });
     }
 }
