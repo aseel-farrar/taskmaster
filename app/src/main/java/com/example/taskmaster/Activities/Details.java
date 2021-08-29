819bb3428c33d7571e35114f1451dfeb1a71a2b7
aseel-farrar <aseelfarrar1@hotmail.com>
2021-08-30 01:31:35 +0300

    cognito

--- app/src/main/java/com/example/taskmaster/Activities/Details.java
+++ app/src/main/java/com/example/taskmaster/Activities/Details.java
@@ -3,9 +3,13 @@ package com.example.taskmaster.Activities;
 import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.amazonaws.mobile.auth.core.IdentityManager;
+import com.amazonaws.mobile.config.AWSConfiguration;
+import com.amplifyframework.core.Amplify;
 import com.example.taskmaster.R;
 
 public class Details extends AppCompatActivity {
@@ -34,6 +38,25 @@ public class Details extends AppCompatActivity {
             Intent goHome = new Intent(Details.this,MainActivity.class);
             startActivity(goHome);
         });
+        // Sign Out
+        findViewById(R.id.imageViewLogOut).setOnClickListener(view -> logout());
 
+    }
+    private void logout() {
+
+        Amplify.Auth.signOut(
+                () -> Log.i("AuthQuickstart", "Signed out successfully"),
+                error -> Log.e("AuthQuickstart", error.toString())
+        );
+
+        // Clear the IdentityManager credentials
+        final IdentityManager idm = new IdentityManager(this, new AWSConfiguration(this));
+        IdentityManager.setDefaultIdentityManager(idm);
+        idm.getUnderlyingProvider().clearCredentials();
+        idm.getUnderlyingProvider().clear();
+        idm.getUnderlyingProvider().setLogins(null);
+
+        Intent goToSignIn = new Intent(Details.this, SignInActivity.class);
+        startActivity(goToSignIn);
     }
 }
---
+++ app/src/main/java/com/example/taskmaster/Activities/SignUpActivity.java
@@ -0,0 +1,64 @@
+package com.example.taskmaster.Activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.amplifyframework.auth.AuthUserAttributeKey;
+import com.amplifyframework.auth.options.AuthSignUpOptions;
+import com.amplifyframework.core.Amplify;
+import com.example.taskmaster.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private static final String TAG = "SignUpActivity";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        setTitle("Sign Up");
+
+        Button signUp = findViewById(R.id.btnlogin);
+        EditText username = findViewById(R.id.eteusername);
+        EditText email = findViewById(R.id.etemail);
+        EditText password = findViewById(R.id.mypass);
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signUp(
+                        username.getText().toString(),
+                        email.getText().toString(),
+                        password.getText().toString());
+            }
+        });
+
+    }
+
+    void signUp(String username, String email, String password) {
+        Amplify.Auth.signUp(
+                username,
+                password,
+                AuthSignUpOptions.builder()
+                        .userAttribute(AuthUserAttributeKey.email(), email)
+                        .build(),
+                success -> {
+                    Log.i(TAG, "signUp successful: " + success.toString());
+                    Intent goToVerification = new Intent(SignUpActivity.this, VerificationActivity.class);
+                    goToVerification.putExtra("username", username);
+                    goToVerification.putExtra("password", password);
+                    startActivity(goToVerification);
+                },
+                error -> {
+                    Log.e(TAG, "signUp failed: " + error.toString());
+                });
+    }
+}
